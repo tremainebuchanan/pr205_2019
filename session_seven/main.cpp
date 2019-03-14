79e8e96bb561be42ed7a2bb06efe5ee740fa6cfd
Tremaine Buchanan <tremainekbuchanan@gmail.com>
2019-03-13 20:59:19 -0500

    lab code complete

---
+++ session_seven/main.cpp
@@ -0,0 +1,19 @@
+//Author: Tremaine Buchanan
+//Session Seven
+//Lab One
+#include <iostream>
+using namespace std;
+
+int main(){
+    int sum = 0;
+    float average = 0;
+    int count;
+    int number = 0;
+    for(count = 0; count < 10; count = count + 1 ){
+        cout << "Enter number " << count + 1 << "--> ";
+        cin >> number;
+        sum = sum + number;
+    }
+    cout << "The average of numbers is " << sum / count << endl;
+    return 0;
+}
